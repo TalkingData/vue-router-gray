1dcb7b83c98ad85e3b99ee93691f39ddac1e743a
wxnet <wxnet@wxnets-MacBook-Pro.local>
2017-05-04 13:40:23 +0800

    feat(gray): vue-router灰度检测插件，兼容vue1.x、vue2.x

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vue-router-gray
 基于vue-router的灰度发布策略
+
+## 使用
+
+```
+import VueRouterGray from 'vue-router-gray';
+const router = new VueRouter({ ... });
+
+router.beforeEach(VueRouterGray());
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = vueRouterGray;
+function vueRouterGray(options) {
+  options = options || {
+    check: function() { return Promise.resolve(false); },
+  };
+  return function() {
+    var to = null;
+    var from = null;
+    var next = null;
+    // vue-router 1.x
+    if (arguments.length === 1 && arguments[0].next) {
+      to = arguments[0].to;
+      from = arguments[0].from;
+      next = arguments[0].next;
+    }
+    // vue-router 2.x
+    if (arguments.length === 3) {
+      to = arguments[0];
+      from = arguments[1];
+      next = arguments[2];
+    }
+    try {
+      options.check().then(function(v) {
+        if (v) {
+          location.href = 'http://www.baidu.com';
+        } else {
+          next();
+        }
+      });
+    } catch(e) {
+      next();
+    }
+  };
+};
